c7f66f989b44b4601da7614e35fe8cdf5752ea0f
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-06-15 19:06:33 +0430

    Transfer Template

---
+++ ServiceHost/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Components
+{
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
